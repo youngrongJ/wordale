dafa28188c45ddb16c3f873b5b09929b37a9be13
youngrongJ <bawla88@gmail.com>
2024-08-08 16:51:27 +0900

    js코드 1차완성

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+const 정답 = "jinny";
+
+let index = 0;
+let attemps = 0;
+
 function gameStart() {
+  const handleEnterKey = () => {
+    //정답확인
+  };
+
   const handleKeyDown = (event) => {
     const key = event.key.toUpperCase();
     const keyCode = event.keyCode;
-    const thisBlock = document.querySelector(".board-column[data-index='00']");
-
     //console.log(event.keyCode);
-    if (keyCode >= 65 && keyCode <= 90) {
+    const thisBlock = document.querySelector(
+      `.board-column[data-index='${attemps}${index}']`
+    );
+
+    if (event.key === "Enter") {
+      handleEnterKey();
+    } else if (index === 5) return;
+    else if (keyCode >= 65 && keyCode <= 90) {
       thisBlock.innerText = key;
+      index++;
     }
+    console.log("키입력", event.key);
   };
 
   window.addEventListener("keydown", handleKeyDown);
